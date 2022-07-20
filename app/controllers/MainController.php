881426f1c9b504c8695a1bf1d9548e65db627a92
RivGames <Igorryd7@gmail.com>
2022-07-20 20:38:52 +0300

    Model

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,12 +1,21 @@
 <?php
 namespace app\controllers;
 
+use app\models\Main;
+
 class MainController extends AppController
 {
     public $layout = 'main';
     public function indexAction()
     {
 //        $this->layout = false;
-        $this->set(['name' => 'Ihor']);
+        $title = 'Main';
+        $model = new Main();
+        $posts = $model->findAll();
+        $post = $model->findOne(1);
+        $post3 = $model->findBySql("SELECT * FROM {$model->table} WHERE id=3");
+        $post2 = $model->findLike('post2','title','posts');
+        debug($post2);
+        $this->set(compact('title','posts','post'));
     }
 }
--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,1 +1,12 @@
-<code><?= __FILE__ ?></code>
+<div class="container">
+    <?php foreach($posts as $post): ?>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <?= $post['title']?>
+        </div>
+        <div class="panel-body">
+            <?= $post['content']?>
+        </div>
+    </div>
+    <?php endforeach;?>
+</div>
